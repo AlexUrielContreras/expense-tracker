ba5d5fe77a8c97b30f0c4a694947dd6d880ded10
Alex Contreras <alex.code29@gmail.com>
2023-03-01 01:51:57 -0600

    feat: create api crud routes for users

---
+++ server/controllers.js/user-controllers.js
@@ -0,0 +1,68 @@
+const User = require('../schemas/index');
+
+const userController = {
+   
+   login(req, res) {
+      // login code
+   },
+
+   createUser({ body }, res) {
+      User.create(body)
+      .then(dbUserData => {
+         res.json(dbUserData)
+      })
+      .catch(err => {
+         console.log(err);
+         res.status(500).json(err)
+      })
+   },
+
+   findAllUsers(req, res) {
+      User.find()
+      .then(dbUserData => {
+         res.json(dbUserData)
+      })
+      .catch(err => {
+         console.log(err)
+         res.status(500)
+      })
+   },
+
+   findUserbyId({ params }, res) {
+      User.findById({
+         _id: params._id
+      })
+      .then(dbUserData => {
+         if (!dbUserData) {
+            res.status(404).json({ message: 'No user with found with that Id'});
+            return
+         }
+
+         res.json(dbUserData)
+      })
+      .catch(err => {
+         console.log(err);
+         res.status(500);
+      })
+   },
+
+   deleteUser({ params }, res) {
+      User.findByIdAndDelete({
+         _id: params._id
+      })
+      .then(dbUserData => {
+         if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with that Id'});
+            return
+         }
+
+         res.status(200).json({ message: 'User has been deleted '})
+      }) 
+      .catch(err => {
+         console.log(err);
+         res.status(500)
+      })
+   }
+};
+
+module.exports = userController
