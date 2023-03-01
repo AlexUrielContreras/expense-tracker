74661f5f3e77f83c5b81998afd48d4abccc7691c
Alex Contreras <alex.code29@gmail.com>
2023-03-01 01:28:53 -0600

    feat: build the user schema

---
+++ server/schemas/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+   
+   firstName: {
+      type: String,
+      trim: true,
+      required: true
+   },
+
+   email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      match:[ /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ ]
+   },
+
+   password: {
+      type: String,
+      trim: true,
+      required: true,
+      minLength: 6
+   },
+
+   budgetAmount: {
+      type: Number,
+      trim: true
+   },
+
+   /*       5 Failed login attempts will lock the user out          */
+
+   loginAttempts: {
+      type: Number,
+      default: 0
+   },
+
+   lastLoginAttempt: {
+      type: Date,
+      default: Date.now()
+   }
+
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
