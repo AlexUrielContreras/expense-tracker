9f56fa1d1704b5baf10e4b27a53b9b3d187ef25f
Alex Contreras <alex.code29@gmail.com>
2023-03-01 01:18:13 -0600

    feat: set up connection to mongodb database and hook it up to express server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const mongoDB = require('./config/connection');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT , () => {
-   console.log(`Now listening on port: ${PORT}`)
+mongoDB.once('open', () => {
+   app.listen(PORT , () => {
+      console.log(`Now listening on port: ${PORT}`)
+   });
 });
