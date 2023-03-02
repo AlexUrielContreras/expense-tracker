d33c0a65046a83e11fb7b0ae3c527387d2bc87f6
Alex Contreras <alex.code29@gmail.com>
2023-03-02 13:50:05 -0600

    feat: create function to sign and verify a token

---
+++ server/utills/auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+function signToken(content) {
+   const token = jwt.sign(content, process.env.TOKEN_SECRET, { expiresIN: '2h' });
+
+   return token
+};
+
+function authenticateToken(req, res, next) {
+   const authHeader = req.header['authorization'];
+
+   // remove Bearer from token 
+   const token = authHeader && authHeader.spit(' ').pop().trim();
+
+   if (token === null) {
+      return res.status(401);
+   };
+
+   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+      if (err) {
+         return res.status(403)
+      }
+
+      req.user = user;
+
+      next()
+   })
+};
+
+
+module.exports = { signToken, authenticateToken }
