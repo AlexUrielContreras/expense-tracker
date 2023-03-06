d908a10324089a98b3bada351fd7d807c008f422
Alex Contreras <alex.code29@gmail.com>
2023-03-06 05:05:56 -0600

    feat: install axios to make call to the apis and created function for jwt authentication

--- client/src/entry-forms/Login.jsx
+++ client/src/entry-forms/Login.jsx
@@ -1,21 +1,53 @@
+import axios from 'axios';
+import { useState } from 'react';
+ 
 function Login() {
+   const [ loginData, setLoginData ] = useState({ email: '', password: ''});
+   const [ errorMessage, setErrorMessage ] = useState('')
 
-   function handleLogin(e) {
+   function handleFormChange(e) {
+      const { name, value} = e.target;
+
+      setLoginData({ ...loginData, [name]: value});
+   }
+
+   async function handleLogin(e) {
       e.preventDefault();
-      console.log('hi')
+      
+      if (loginData.password) {
+         try {
+            await axios({
+               method: 'post',
+               url: 'api/user/login',
+               data: JSON.stringify(loginData),
+               headers: {
+                  'Content-Type' : 'application/json'
+               }
+            }) 
+         } catch (err) {
+            console.log(err)
+            setErrorMessage('Incorrect Credentials')
+         }
+      }
    };
 
    return (
       <form onSubmit={handleLogin} className='form-container'>
          <div className='form-inputs-container'>
             <label htmlFor='email'>Email</label>
-            <input type='email' name='email' id='email' />
+            <input type='email' name='email' onChange={handleFormChange} id='email' />
          </div>
 
          <div className='form-inputs-container'>
             <label htmlFor='password'>Password</label>
-            <input type='password' name='password' id='password' />
+            <input type='password' name='password' onChange={handleFormChange} id='password' />
          </div>
+
+         {errorMessage && 
+            <div>
+               <p className='error-message'>{errorMessage}</p>
+            </div>
+         }
 
          <button type='submit'>Login</button>
       </form>
