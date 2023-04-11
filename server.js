a2fc5db56b0a3a543af9e53dc09c3e14c20d17de
Alex Contreras <alex.code29@gmail.com>
2023-04-11 14:08:50 -0500

    fix: heroku routing error

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, './client/build', 'index.html'))
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, './client/build', 'index'))
 });
 
 app.use(routes);
