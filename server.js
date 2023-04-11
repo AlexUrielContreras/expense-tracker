7bcfaf3dae76a677097a6ec1e1722a0d9ecb9276
Alex Contreras <alex.code29@gmail.com>
2023-04-11 13:17:37 -0500

    fix: heroku client side rendering

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, './client/build', 'index.html'))
+});
 
 app.use(routes);
 
